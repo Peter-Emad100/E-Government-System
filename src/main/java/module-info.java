95003f062b4367d41112bf602170a7d0cca456f3
Peter Emad <139802433+Peter-Emad100@users.noreply.github.com>
2024-11-23 21:05:15 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.egoverment {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.egoverment to javafx.fxml;
+    exports com.example.egoverment;
+}
