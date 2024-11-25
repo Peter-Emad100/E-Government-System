e0b8bdaf3e49ec3580a95ffab213cc922873ad47
Hana <nona.mazen.789@gmail.com>
2024-11-25 11:50:19 +0200

    testing....

--- src/main/java/com/example/egoverment/Minister.java
+++ src/main/java/com/example/egoverment/Minister.java
@@ -1,7 +1,7 @@
 package com.example.egoverment;
 import java.time.LocalDate;
 public class Minister extends Human{
-    private final LocalDate startingDate;
+    private LocalDate startingDate;
     Minister(String name,String id){
         super(name,id);
         startingDate=LocalDate.now();
