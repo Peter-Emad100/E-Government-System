b98063c1ebd2b4ed93c7e5453e1d6d685da7667d
farahweaam <fffsss.weaam@gmail.com>
2024-12-06 17:22:26 +0200

    FORCE PUSH

--- src/main/java/com/example/egoverment/HelloApplication.java
+++ src/main/java/com/example/egoverment/HelloApplication.java
@@ -7,11 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class HelloApplication extends Application {
    @Override
     public void start(Stage primaryStage) throws Exception {
       try {
-            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("Add_Employee.fxml"));
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("User_MainMenu.fxml"));
            Pane root = (Pane) loader.load();
            //Create a scene and set the stage
           System.out.println("in");
@@ -21,7 +23,7 @@ public class HelloApplication extends Application {
            primaryStage.show();
 
            //Access the controller if needed
-           AddEmpController controller = loader.getController();
+           User_Menu_Controller controller = loader.getController();
       } catch (Exception e) {
            e.printStackTrace();
            System.out.println("Failed to load FXML. Check the file path.");
@@ -41,6 +43,14 @@ public class HelloApplication extends Application {
     public static void main (String[] args){
        FileHelper.retrieveAll();
        launch(args);
+//        ArrayList<Ministry> ministries= new ArrayList<>();
+//        ministries=MinistriesCollection.getMinistries();
+//        int length = ministries.size()-1;
+//        for(int i=0;i<length;i++)
+//        {
+//            System.out.println(i + ministries.get(i).getMinistryName());
+//        }
        FileHelper.saveAll();
+
     }
 }
