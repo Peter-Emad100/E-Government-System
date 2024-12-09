6b64145f984f937ce8f1ac8f1164e12c9c78dc14
Peter Emad <139802433+Peter-Emad100@users.noreply.github.com>
2024-12-09 02:16:37 +0200

    .

--- src/main/java/com/example/egoverment/MinistriesCollection.java
+++ src/main/java/com/example/egoverment/MinistriesCollection.java
@@ -19,31 +19,6 @@ public class  MinistriesCollection {
     private final static String savingName="ministries";
     private static ArrayList<Ministry> minis = new ArrayList<Ministry>();
 
-    /*static{
-        minis.add(new Ministry("Ministry of Interior",i++));
-        minis.add(new Ministry("Ministry of Education",i++));
-        minis.add(new Ministry("Ministry of Finance",i++));
-        minis.add(new Ministry("Ministry of Transportation",i++));
-        //minis.add(new MinistryOfElectricity("Ministry Electricity and Energy",i++));
-        minis.add(new MinistryOfHealthAndPopulation("Ministry of Health and population",i++));
-
-    };*/
-    /*private static ArrayList<Department> dep =new ArrayList<>();
-    static {
-        dep.add(new Department("department of national budget"));
-        dep.add(new Department("department of treasury operations"));
-        dep.add(new Department("department of information technology management"));
-
-    }
-    public static Department findDepartmentByName(String name) {
-        for (Department departments : dep) {
-            if (departments.getDepartmentName ().equalsIgnoreCase(name)) {
-                return departments;
-            }
-        }
-        return null;
-    }*/
-
     public static void add (String name){
         minis.add(new Ministry(name));
     }
