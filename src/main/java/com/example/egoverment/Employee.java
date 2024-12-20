048fad51392ae297a8a08ac3dbdaecac11e239cc
Hana <nona.mazen.789@gmail.com>
2024-12-20 12:42:19 +0200

    Final InshAllah

--- src/main/java/com/example/egoverment/Employee.java
+++ src/main/java/com/example/egoverment/Employee.java
@@ -4,12 +4,20 @@ import java.time.LocalDate;
 
 public class Employee extends Human{
     private String startingDate;
+    private Department department;
 
-    public String getStartingDate(){
-        return startingDate;
-    }
-    Employee(String name, int id){
+    public Employee(String name, int id){
         super(name,id);
         startingDate=LocalDate.now().toString();
     }
+    public String getStartingDate(){
+        return startingDate;
+    }
+    public String getdapname(){
+        return department.getDepartmentName();
+    }
+    public void inDepartment(Department d){
+       this.department=d;
+    }
+
 }
